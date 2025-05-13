6fe0d518e8da55c7e145c0ca7eddcf3ea8b25bee
Brian <brianyin09@gmail.com>
2025-05-13 14:20:59 -0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Brian Yin on 5/13/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
